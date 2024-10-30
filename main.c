fff5a8e6328430dd3a7f685420f2f8780116706f
rahul <ee24mt007@iitdh.ac.in>
2024-10-31 00:26:48 +0530

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+
+
+void UART0_Init(void);
+char UART0_Received(void);
+void UART0_Send(char data);
+void LED_Init(void);
+void LED_ON_OFF(char choice);
+
+int main(void) {
+
+    }
+}
